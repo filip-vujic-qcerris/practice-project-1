6f3674149cc0b6e71ed548b3435482dcbb5fb793
filip-vujic-qcerris <filip.vujic@qcerris.com>
2022-11-14 13:24:06 +0100

    Init commit

---
+++ src/main/java/com/filipvujic/practiceproject1/PracticeProject1Application.java
@@ -0,0 +1,15 @@
+package com.filipvujic.practiceproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeProject1Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PracticeProject1Application.class, args);
+
+		System.out.println("Test test.");
+	}
+}
---
+++ src/test/java/com/filipvujic/practiceproject1/PracticeProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filipvujic.practiceproject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
